71ff62f67841cf0438ea22641eef457c829aeb1f
k1LoW <k1lowxb@gmail.com>
2022-06-16 15:51:24 +0900

    Support apply updating of findings workflow status

--- sechub/apply.go
+++ sechub/apply.go
@@ -6,10 +6,13 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
 	"github.com/aws/aws-sdk-go-v2/service/securityhub"
 	"github.com/aws/aws-sdk-go-v2/service/securityhub/types"
 	"github.com/rs/zerolog/log"
 )
+
+const noteUpdateBy = "control-controls"
 
 func (sh *SecHub) Apply(ctx context.Context, cfg aws.Config, reason string) error {
 	region := cfg.Region
@@ -66,6 +69,10 @@ func (sh *SecHub) Apply(ctx context.Context, cfg aws.Config, reason string) erro
 	for _, std := range diff.Standards {
 		key := std.Key
 		s := stds.findByKey(key)
+		a, err := arn.Parse(*s.subscriptionArn)
+		if err != nil {
+			return err
+		}
 		if s == nil {
 			return fmt.Errorf("could not find standard on %s: %s", region, key)
 		}
@@ -102,62 +109,131 @@ func (sh *SecHub) Apply(ctx context.Context, cfg aws.Config, reason string) erro
 			}
 		}
 
-		// Standards.Controls
-		if std.Controls == nil {
+		if std.Controls == nil && std.Findings == nil {
 			log.Debug().Str("Region", region).Str("Standard", key).Msg("Skip controls as there is no difference")
 			continue
 		}
-		cs, err := ctrls(ctx, c, s.subscriptionArn)
-		if err != nil {
-			return err
-		}
-		for _, id := range std.Controls.Enable {
-			arn, ok := cs.arns[id]
-			if !ok {
-				log.Debug().Str("Region", region).Str("Standard", key).Str("Control", id).Msg("Skip control")
-				continue
-			}
-			if contains(cs.Enable, id) {
-				continue
-			}
-			update = true
-			log.Info().Str("Region", region).Str("Standard", key).Str("Control", id).Msg("Enable control")
-			if _, err := c.UpdateStandardsControl(ctx, &securityhub.UpdateStandardsControlInput{
-				StandardsControlArn: arn,
-				ControlStatus:       types.ControlStatusEnabled,
-			}); err != nil {
+
+		// Standards.Controls
+		if std.Controls != nil {
+			cs, err := ctrls(ctx, c, s.subscriptionArn)
+			if err != nil {
 				return err
 			}
-			// ref: https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/securityhub@v1.20.0#pkg-overview
-			// * UpdateStandardsControl - RateLimit of 1 request per second, BurstLimit of 5 requests per second.
-			time.Sleep(1 * time.Second)
+			for _, id := range std.Controls.Enable {
+				arn, ok := cs.arns[id]
+				if !ok {
+					log.Debug().Str("Region", region).Str("Standard", key).Str("Control", id).Msg("Skip control")
+					continue
+				}
+				if contains(cs.Enable, id) {
+					continue
+				}
+				update = true
+				log.Info().Str("Region", region).Str("Standard", key).Str("Control", id).Msg("Enable control")
+				if _, err := c.UpdateStandardsControl(ctx, &securityhub.UpdateStandardsControlInput{
+					StandardsControlArn: arn,
+					ControlStatus:       types.ControlStatusEnabled,
+				}); err != nil {
+					return err
+				}
+				// ref: https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/securityhub@v1.20.0#pkg-overview
+				// * UpdateStandardsControl - RateLimit of 1 request per second, BurstLimit of 5 requests per second.
+				time.Sleep(1 * time.Second)
+			}
+			for _, d := range std.Controls.Disable {
+				id := d.Key.(string)
+				if d.Value.(string) != "" {
+					reason = d.Value.(string)
+				}
+				arn, ok := cs.arns[id]
+				if !ok {
+					log.Debug().Str("Region", region).Str("Standard", key).Str("Control", id).Msg("Skip control")
+					continue
+				}
+				if containsMapSlice(cs.Disable, id, reason) {
+					continue
+				}
+				update = true
+				log.Info().Str("Region", region).Str("Standard", key).Str("Control", id).Str("Reason", reason).Msg("Disable control")
+				if _, err := c.UpdateStandardsControl(ctx, &securityhub.UpdateStandardsControlInput{
+					StandardsControlArn: arn,
+					ControlStatus:       types.ControlStatusDisabled,
+					DisabledReason:      &reason,
+				}); err != nil {
+					return err
+				}
+				// ref: https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/securityhub@v1.20.0#pkg-overview
+				// * UpdateStandardsControl - RateLimit of 1 request per second, BurstLimit of 5 requests per second.
+				time.Sleep(1 * time.Second)
+			}
 		}
-		for _, d := range std.Controls.Disable {
-			id := d.Key.(string)
-			if d.Value.(string) != "" {
-				reason = d.Value.(string)
-			}
-			arn, ok := cs.arns[id]
-			if !ok {
-				log.Debug().Str("Region", region).Str("Standard", key).Str("Control", id).Msg("Skip control")
-				continue
-			}
-			if containsMapSlice(cs.Disable, id, reason) {
-				continue
-			}
-			update = true
-			log.Info().Str("Region", region).Str("Standard", key).Str("Control", id).Str("Reason", reason).Msg("Disable control")
-			if _, err := c.UpdateStandardsControl(ctx, &securityhub.UpdateStandardsControlInput{
-				StandardsControlArn: arn,
-				ControlStatus:       types.ControlStatusDisabled,
-				DisabledReason:      &reason,
-			}); err != nil {
+
+		// Standards.Findings
+		if std.Findings != nil {
+			cs, err := ctrls(ctx, c, s.subscriptionArn)
+			if err != nil {
 				return err
 			}
-			// ref: https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/securityhub@v1.20.0#pkg-overview
-			// * UpdateStandardsControl - RateLimit of 1 request per second, BurstLimit of 5 requests per second.
-			time.Sleep(1 * time.Second)
+			for _, fg := range std.Findings {
+				for _, r := range fg.Resources {
+					aa, err := arn.Parse(r.Arn)
+					if err != nil {
+						return err
+					}
+					if region != "" && aa.Region != "" && aa.Region != region {
+						continue
+					}
+					cArn, ok := cs.arns[fg.ControlID]
+					if !ok {
+						return fmt.Errorf("not found: %s", fg.ControlID)
+					}
+					got, err := c.GetFindings(ctx, &securityhub.GetFindingsInput{
+						Filters: &types.AwsSecurityFindingFilters{
+							AwsAccountId:  []types.StringFilter{{Comparison: types.StringFilterComparisonEquals, Value: aws.String(a.AccountID)}},
+							ResourceId:    []types.StringFilter{{Comparison: types.StringFilterComparisonEquals, Value: aws.String(r.Arn)}},
+							ProductName:   []types.StringFilter{{Comparison: types.StringFilterComparisonEquals, Value: aws.String("Security Hub")}},
+							ProductFields: []types.MapFilter{{Comparison: types.MapFilterComparisonEquals, Key: aws.String("StandardsControlArn"), Value: cArn}},
+						},
+					})
+					if err != nil {
+						return err
+					}
+					if len(got.Findings) != 1 {
+						return fmt.Errorf("not found: %s", r.Arn)
+					}
+					gotFg := got.Findings[0]
+					status := string(gotFg.Workflow.Status)
+					note := ""
+					if gotFg.Note != nil && gotFg.Note.Text != nil {
+						note = *gotFg.Note.Text
+					}
+					if r.Status != status || r.Note != note {
+						log.Info().Str("Region", region).Str("Standard", key).Str("Control", fg.ControlID).Str("Resource ID", r.Arn).Str("Status", r.Status).Str("Note", r.Note).Msg("Change workfow status")
+
+						input := &securityhub.BatchUpdateFindingsInput{
+							FindingIdentifiers: []types.AwsSecurityFindingIdentifier{{
+								Id:         gotFg.Id,
+								ProductArn: gotFg.ProductArn,
+							}},
+							Workflow: &types.WorkflowUpdate{
+								Status: types.WorkflowStatus(r.Status),
+							},
+						}
+						if r.Note != "" {
+							input.Note = &types.NoteUpdate{
+								Text:      aws.String(r.Note),
+								UpdatedBy: aws.String(noteUpdateBy),
+							}
+						}
+						if _, err := c.BatchUpdateFindings(ctx, input); err != nil {
+							return err
+						}
+					}
+				}
+			}
 		}
+
 	}
 
 	if !update {
